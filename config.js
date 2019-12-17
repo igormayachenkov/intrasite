9245230900cde4e031c6bea26a8d7036889e69ac
igor <igor.mayachenkov@hiq.ru>
2019-12-17 16:12:27 +0300

    start https with passphrase; config.js added

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+	HTTPS_KEY  	: '/Users/jenkins/intrasite/cert/hiqruprivate.key',
+	HTTPS_CRT  	: '/Users/jenkins/intrasite/cert/hiqrupublic.crt',
+	HTTPS_CA  	: '/Users/jenkins/intrasite/cert/intermediate.crt'
+}
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 "use strict"
-const opuntia = require("opuntia");
-const ApiError = opuntia.error.ApiError;
+const fs 		= require("fs");
+const opuntia 	= require("opuntia");
+const ApiError 	= opuntia.error.ApiError;
 const Database  = require("./database.js");
 const scheme    = require("./scheme.js");
 const info  	= require("./info.js");
@@ -17,18 +18,15 @@ global.config = {
 	}
 };
 
-// var PORT = process.argv[2];
-// if(!PORT) PORT = 80
-
 //-------------------------------------------------------------------------------------------------
 // CREATE & CONFIG API SERVER
 var router = {
-	// $title: "Public WEB server",
-	// $descr: "Put intrasite-react/build here",
-	// _files:	__dirname+'/build/',
-	// _default:'index.html',
-	// _404:	 'index.html', // for React router
-	// h_get: 	opuntia.files.get,
+	$title: "Public WEB server",
+	$descr: "Put intrasite-react/build here",
+	_files:	__dirname+'/build/',
+	_default:'index.html',
+	_404:	 'index.html', // for React router
+	h_get: 	opuntia.files.get,
 
 	// The router
 	router: {
@@ -59,21 +57,55 @@ var router = {
 	}
 };
 
-// CREATE & START API SERVER
-var server = new opuntia.Server(router, {
-		PROTOCOL   	: 'http:',
-		PORT       	: 8080
-	}
-);
-server.listen(async function(){
-	
-	// Create db connection
-	var database = new Database();
-	await database.connect('intrasite');
-	await scheme.verifyDatabase(database); // check/create tables
-	router.api._db = database
+const startServer = (protocol, port, options)=>{
+	var server = new opuntia.Server(router, {
+		PROTOCOL   	: protocol,
+		PORT       	: port
+	});
+	server.start(options, async function(){
+		// Create db connection
+		var database = new Database();
+		await database.connect('intrasite');
+		await scheme.verifyDatabase(database); // check/create tables
+		router.api._db = database
 
-	// START STATIC WEB SERVER
-	var testUrl   = "http://localhost:"+server.config.PORT+"/doc";
-	console.log("Open the next URL for test:\n"+testUrl);
-});
+		// START STATIC WEB SERVER
+		var testUrl   = protocol+"//localhost:"+server.config.PORT+"/doc";
+		console.log("Open the next URL for test:\n"+testUrl);
+	});
+}
+
+// CREATE & START API SERVER
+// Release or Development?
+if(process.argv[2]=='dev'){
+	console.log('DEVELOPMENT MODE')
+	startServer('http:', 8080, null)
+}else{
+	// RELEASE
+	// Load config
+	const config = require("./config.js");
+	if(!config.HTTPS_KEY || !config.HTTPS_CRT) 
+		throw 'Error: config.HTTPS_KEY or config.HTTPS_CRT is undefined';
+
+	// Load certificates from files
+	var options = {
+		key:  fs.readFileSync(config.HTTPS_KEY), //'private-key.pem'),
+		cert: fs.readFileSync(config.HTTPS_CRT)  //'certificate.pem')
+	};
+	if(config.HTTPS_CA)
+		options.ca = fs.readFileSync(config.HTTPS_CA)
+	
+	// Input the pathphrase	
+	var readline = require('readline');// https://nodejs.org/api/readline.html
+	var rl = readline.createInterface({
+		input : process.stdin,
+		output: process.stdout
+	});
+	console.log('Input the RSA-key passphrase:');
+	rl.on('line', function(line){
+		rl.close();
+		// Start server
+		options.passphrase = line;
+		startServer('https:', 8080, options)
+	})
+}
